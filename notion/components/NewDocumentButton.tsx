a58ffa22f654dd012a7a4c7f43bbe14df2c32dce
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-12-02 21:30:45 +0530

    newDoc updated

--- notion/components/NewDocumentButton.tsx
+++ notion/components/NewDocumentButton.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+'use client'
+
+
+import { useTransition } from 'react'
 import { Button } from './ui/button'
+import { useRouter } from 'next/navigation';
+
 
 function NewDocumentButton() {
+
+  const [isPending, startTransition] = useTransition();//in case of any async behaviour, which is not media, we can wrap it in this hook
+  const router = useRouter 
+
+  const handleCreateNewDocument = () => {
+    startTransition(async () => {
+      const { docId } = await createDocument();
+      router.push(`/doc/${docId}`)
+    })
+    
+  }
+
   return (
-    <Button>
-        New Document
+    <Button onClick={handleCreateNewDocument} disabled={isPending}>
+        {isPending ? "Creating..." : "New Document"}
     </Button>
   )
 }
