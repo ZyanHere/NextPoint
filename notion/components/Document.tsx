c36d40f8257ba3a8d72a3db65e4c07bfc883d7f5
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-12-09 22:42:44 +0530

    homepage setup

---
+++ notion/components/Document.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+function Document({id}: {id}) {
+  return (
+    <div>
+      <div>
+        <form action="">
+          {/* update title */}
+
+          {/* if */}
+        </form>
+      </div>
+      <div>
+        {/* manageUsers */}
+{/* 
+        avatars */}
+      </div>
+
+      {/* Collaborative editor */}
+    </div>
+  )
+}
+
+export default Document
