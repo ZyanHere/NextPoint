53a3d43013dfa5a95b8d84f4c16945768d5120bb
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-12-17 20:44:50 +0530

    title func added

--- notion/components/Document.tsx
+++ notion/components/Document.tsx
@@ -1,11 +1,45 @@
 "use client"
 
-function Document({id}: {id}) {
+import { useEffect, useState, useTransition } from "react"
+import { Input } from "./ui/input"
+import { Button } from "./ui/button";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+
+function Document({id}: {id: string}) {
+
+  const [data, loading, error] = useDocumentData(doc(db, "document", id));
+  const [input, setInput] = useState("");
+  const [isUpdating, startTransition] = useTransition();
+
+  useEffect(() => {
+    if (data) {
+      setInput(data.title);
+    }
+  }, [data])
+
+  const updateTitle = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (input.trim()){
+      startTransition(async () => {
+        await updateDoc(doc(db, "documents", id), {
+          title: input,
+        })
+      })
+    }
+  }
+
   return (
     <div>
-      <div>
-        <form action="">
+      <div className=" flex max-w-6xl mx-auto justify-between pb-5">
+        <form className=" flex flex-1 space-x-2" onSubmit={updateTitle}>
           {/* update title */}
+          <Input value={input} onChange={(e)=> setInput(e.target.value)}/>
+          <Button disabled={isUpdating} type="submit">
+            {isUpdating ? "Updating" : "Update"}
+          </Button>
 
           {/* if */}
         </form>
