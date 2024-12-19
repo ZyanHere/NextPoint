c24532f0b66d7b6c2bfddb2a17935a4d69b3b772
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-12-19 23:14:18 +0530

    livecursor provider added

--- notion/components/RoomProvider.tsx
+++ notion/components/RoomProvider.tsx
@@ -5,6 +5,7 @@ import {
     RoomProvider as RoomProviderWrapper,
 } from "@liveblocks/react/suspense"
 import LoadingSpinner from "./LoadingSpinner"
+import LiveCursorProvider from "./LiveCursorProvider"
 
 function RoomProvider({roomId, children}: {
     roomId: string,
@@ -18,7 +19,7 @@ function RoomProvider({roomId, children}: {
     }}
    >
     <ClientSideSuspense fallback={<LoadingSpinner/>}>
-      {children}
+      <LiveCursorProvider>{children}</LiveCursorProvider>
     </ClientSideSuspense>
    </RoomProviderWrapper>
   )
