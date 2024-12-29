98bf9637815a34fdbffc337cbd94fa88c95d053b
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-12-29 20:23:05 +0530

    delete doc server action added

--- notion/actions/actions.ts
+++ notion/actions/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { adminDb } from "@/firebase-admin";
+import liveblocks from "@/lib/liveblocks";
 import { auth } from "@clerk/nextjs/server";
 
 // this server action will send the user to login if he's logged out and tries to create new doc
@@ -28,5 +29,37 @@ export async function createNewDocument() {
       roomId: docRef.id,
     });
 
-    return {docId: docRef.id,}
+  return { docId: docRef.id };
+}
+
+export async function deleteDocument(roomId: string) {
+  auth().protect();
+
+  try {
+
+    //delete doc ref itself
+    await adminDb.collection("documents").doc(roomId).delete();
+
+    const query = await adminDb
+      .collectionGroup("rooms")
+      .where("roomId", "==", roomId)
+      .get();
+
+    const batch = adminDb.batch();
+
+    //delete toom ref in user's collection
+    query.docs.forEach((doc) => {
+      batch.delete(doc.ref);
+    })
+
+    await batch.commit();
+
+    //delete room in liveblocks
+    await liveblocks.deleteRoom(roomId);
+    return { success: true };
+
+  } catch (error) {
+    console.error(error);
+    return { success: false };
+  }
 }
