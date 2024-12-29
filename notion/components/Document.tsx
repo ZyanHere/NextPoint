bb023f5404451e6647953e8c8bb73fc04f198a2c
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-12-29 19:30:39 +0530

    useOwner hook added

--- notion/components/Document.tsx
+++ notion/components/Document.tsx
@@ -7,12 +7,14 @@ import { doc, updateDoc } from "firebase/firestore";
 import { db } from "@/firebase";
 import { useDocumentData } from "react-firebase-hooks/firestore";
 import Editor from "./Editor";
+import useOwner from "@/lib/useOwner";
 
 function Document({id}: {id: string}) {
 
   const [data, loading, error] = useDocumentData(doc(db, "document", id));
   const [input, setInput] = useState("");
   const [isUpdating, startTransition] = useTransition();
+  const isOwner = useOwner();
 
   useEffect(() => {
     if (data) {
@@ -33,7 +35,7 @@ function Document({id}: {id: string}) {
   }
 
   return (
-    <div>
+    <div className=" flex-1 h-full bg-white p-5"> 
       <div className=" flex max-w-6xl mx-auto justify-between pb-5">
         <form className=" flex flex-1 space-x-2" onSubmit={updateTitle}>
           {/* update title */}
